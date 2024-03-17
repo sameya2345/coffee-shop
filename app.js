9907ad3de4d6808fb6ffc24ffc7682884f64f2b1
sameya2345 <sameyachowdhury33@gmail.com>
2024-03-17 23:24:13 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .flex .navbar');
+
+menu. onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+    
+}
+window. onscroll = () =>{
+    menu.classList.remov('fa-times');
+    navbar.classList.remov('active');
+    
+}
+
